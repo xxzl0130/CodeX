524366dc30c71e1b270132d3be366ecff8aa7c03
xxzl0130 <zaxs0130@gmail.com>
2020-03-27 10:42:59 +0800

    调整默认排序

--- CodeX/AboutDialog.cpp
+++ CodeX/AboutDialog.cpp
@@ -5,7 +5,7 @@
 AboutDialog::AboutDialog(QWidget *parent)
 	: QDialog(parent),
 	ui(new Ui::AboutDialog()),
-	version(1,8,1),
+	version(1,8,2),
 	accessManager_(new QNetworkAccessManager(this))
 {
 	ui->setupUi(this);
