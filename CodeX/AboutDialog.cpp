b7d1dda4f872fd614b725696652fd31b94d3385b
xxzl0130 <zaxs0130@gmail.com>
2021-05-09 21:40:08 +0800

    适配gitee

--- CodeX/AboutDialog.cpp
+++ CodeX/AboutDialog.cpp
@@ -4,7 +4,7 @@
 
 AboutDialog::AboutDialog(QWidget *parent)
 	: QDialog(parent),
-	version(2,1,2),
+	version(2,2,0),
 	ui(new Ui::AboutDialog()),
 	accessManager_(new QNetworkAccessManager(this))
 {
@@ -25,45 +25,48 @@ AboutDialog::~AboutDialog()
 
 void AboutDialog::checkUpdate()
 {
-#ifdef _DEBUG
-	QNetworkProxy proxy;
-	proxy.setType(QNetworkProxy::Socks5Proxy);
-	proxy.setHostName("127.0.0.1");
-	proxy.setPort(10808);
-	QNetworkProxy::setApplicationProxy(proxy);
-#endif
 	request_.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded; charset=UTF-8");
 	QSslConfiguration config = request_.sslConfiguration();
 	config.setPeerVerifyMode(QSslSocket::VerifyNone);
 	config.setProtocol(QSsl::SecureProtocols);
 	request_.setSslConfiguration(config);
-	request_.setUrl(QUrl("https://api.github.com/repos/xxzl0130/CodeX/releases/latest"));
+	request_.setUrl(QUrl("https://gitee.com/api/v5/repos/zaxs0130/CodeX/releases"));
 	auto onReply =
 		QObject::connect(
 			accessManager_,
 			&QNetworkAccessManager::finished,
 			[&](QNetworkReply* reply)
 			{
-				if (reply->error() != QNetworkReply::NoError)
-				{
-					reply->deleteLater();
-					return;
-				}
-				auto json = QJsonDocument::fromJson(reply->readAll()).object();
-				auto ver = json["tag_name"].toString("v1.0").toLower().split("v");
-				if (ver.size() > 1)
-				{
-					auto newVersion = QVersionNumber::fromString(ver[1]);
+				try {
+					if (reply->error() != QNetworkReply::NoError)
+					{
+						reply->deleteLater();
+						return;
+					}
+					auto json = QJsonDocument::fromJson(reply->readAll()).array()[0].toObject();
+					auto ver = json["tag_name"].toString("v1.0").toLower().split("v", QString::SkipEmptyParts);
+					if (ver.empty())
+					{
+						reply->deleteLater();
+						return;
+					}
+					auto newVersion = QVersionNumber::fromString(ver[0]);
 					if (newVersion > version)
 					{
-						auto ret = QMessageBox::information(nullptr, u8"提示", u8"检测到新版本，是否下载？", QMessageBox::Yes | QMessageBox::No);
+						auto ret = QMessageBox::information(nullptr, u8"提示", u8"检测到新版本，是否下载？",
+							QMessageBox::Yes | QMessageBox::No);
 						if (ret == QMessageBox::Yes)
 						{
-							QDesktopServices::openUrl(QUrl("https://github.com/xxzl0130/CodeX/releases"));
+							QDesktopServices::openUrl(
+								QUrl(json["assets"].toArray()[0].toObject()["browser_download_url"].toString()));
 						}
 					}
+					reply->deleteLater();
 				}
-				reply->deleteLater();
+				catch (std::exception e)
+				{
+					reply->deleteLater();
+				}
 			});
 	accessManager_->get(request_);
 }
